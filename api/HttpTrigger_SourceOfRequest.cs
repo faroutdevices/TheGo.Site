cc96a7e37d5c356371e1385bd2dfe53bbb9bacc2
uniqueusername10@gmail.com <uniqueusername10@gmail.com>
2021-07-15 15:25:54 -0500

    updated cookie url to www

--- api/HttpTrigger_SourceOfRequest.cs
+++ api/HttpTrigger_SourceOfRequest.cs
@@ -54,7 +54,7 @@ namespace TheGoSite.Function
                 // Set a cookie
                 CookieOptions option = new CookieOptions();
                 option.Expires = DateTime.Now.AddYears(1);
-                option.Domain = "thego.site";
+                option.Domain = "www.thego.site";
                 option.HttpOnly = true;
                 //// A little non logical way to actually get the HttpResponse (from the HttpRequest and its HttpContext)
                 req.HttpContext.Response.Cookies.Append("BeenHereBefore", "true", option);
