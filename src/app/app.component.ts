be5cbb1c14045191b28eecf867e4793d8348b709
CJ <uniqueusername10@gmail.com>
2021-07-18 16:10:19 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/rrouter';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 //import { environment } from '../environments/environment';
 //import { DeviceDetectorService } from 'ngx-device-detector';
