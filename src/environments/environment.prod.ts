bc55ae7680a63be63372b1e32ab03978b7de4eb5
CJ <uniqueusername10@gmail.com>
2021-08-17 15:48:01 -0500

    Bug fix - updated path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlFunctions1: "https://www.thego.site/api/HttpTrigger_SourceOfRequest",
+  urlFunctions1: "https://www.thego.site",
   //urlFunctions2: "azurestaticapps.net"
 };
