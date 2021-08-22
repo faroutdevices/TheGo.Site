43081dc123d4beaa328457b69a9cf40220af5f83
CJ <uniqueusername10@gmail.com>
2021-08-21 23:07:35 -0500

    added classes

---
+++ src/app/QuestionAndAnswer.ts
@@ -0,0 +1,8 @@
+export class QuestionAndAnswer
+{
+  TheUser!: String;
+  Question!: String;
+  Answer!: String;
+  AnswerType!: number;
+  Timestamp!: Date;
+}
---
+++ src/app/SourceOfRequestRecord.ts
@@ -0,0 +1,7 @@
+export class SourceOfRequestRecord
+{
+  TheUser!: String;
+  TheUserHuman!: String;
+  LastVisit!: String;
+  FirstVisit!: String;
+}
